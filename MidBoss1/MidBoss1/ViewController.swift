e7494ceef7b8bebf4f82af9d1b76f3b8fcf2ea5a
OscarDPan <oscarpan@bu.edu>
2016-08-29 00:07:34 -0700

    first push midboss1

---
+++ MidBoss1/MidBoss1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MidBoss1
+//
+//  Created by Zixuan Pan on 8/28/16.
+//  Copyright © 2016 Oscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var outputText: UILabel!
+
+    @IBOutlet var inputText: UITextField!
+    @IBAction func updateInput(sender: AnyObject) {
+        outputText.text = inputText.text
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
