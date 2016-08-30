276d248e46e025d39f978f9e5c578b9e2e40814d
opan <oscarpan@bu.edu>
2016-08-29 23:56:29 -0700

    First commit to midboss

---
+++ MidBoss1/MidBoss1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MidBoss1
+//
+//  Created by Oscar Pan on 8/29/16.
+//  Copyright © 2016 Oscar Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var outputText: UILabel!
+    @IBAction func updateText(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
